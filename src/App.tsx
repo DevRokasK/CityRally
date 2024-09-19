3acd07d735dd811dcb5c4dc14e00ffa3857c1644
Rokas Kašinskas <yourokas@gmail.com>
2024-09-19 12:32:17 +0300

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -41,7 +40,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
